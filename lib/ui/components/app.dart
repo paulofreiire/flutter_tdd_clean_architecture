c3c0be4f3607d3ae6b88629587247362d3575a8d
Paulo Freire <paulo.freiire@hotmail.com>
2021-11-30 01:51:50 -0300

    add loginPage layout

--- lib/ui/components/app.dart
+++ lib/ui/components/app.dart
@@ -5,9 +5,40 @@ import '../pages/pages.dart';
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final primaryColor = Color.fromRGBO(136, 14, 79, 1);
+    final primaryColorDark = Color.fromRGBO(96, 0, 30, 1);
+    final primaryColorLight = Color.fromRGBO(188, 71, 123, 1);
+
     return MaterialApp(
       title: '4Dev',
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: primaryColor,
+        primaryColorDark: primaryColorDark,
+        primaryColorLight: primaryColorLight,
+        accentColor: primaryColor,
+        backgroundColor: Colors.white,
+        textTheme: TextTheme(
+          headline1: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+              color: primaryColorDark),
+        ),
+        inputDecorationTheme: InputDecorationTheme(
+            enabledBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: primaryColorLight)),
+            alignLabelWithHint: true),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ButtonStyle(
+            foregroundColor: MaterialStateProperty.all(Colors.white),
+            backgroundColor:
+                MaterialStateProperty.all(Theme.of(context).primaryColor),
+            shape: MaterialStateProperty.all(RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+            )),
+          ),
+        ),
+      ),
       home: LoginPage(),
     );
   }
--- lib/ui/pages/login_page.dart
+++ lib/ui/pages/login_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_tdd_clean/ui/components/headline1.dart';
+import '../components/login_header.dart';
 
 class LoginPage extends StatelessWidget {
   @override
@@ -6,33 +8,51 @@ class LoginPage extends StatelessWidget {
     return Scaffold(
       body: SingleChildScrollView(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Container(
-              child: Image(image: AssetImage('lib/ui/assets/logo.png')),
-            ),
-            Text('Login'.toUpperCase()),
-            Form(
-              child: Column(
-                children: <Widget>[
-                  TextFormField(
-                    decoration: InputDecoration(
-                        labelText: 'Email', icon: Icon(Icons.email)),
-                    keyboardType: TextInputType.emailAddress,
-                  ),
-                  TextFormField(
-                    decoration: InputDecoration(
-                        labelText: 'Senha', icon: Icon(Icons.lock)),
-                    obscureText: true,
-                  ),
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: Text('Entrar'.toUpperCase()),
-                  ),
-                  ElevatedButton(
+            LoginHeader(),
+            Headline1(text: 'Login',),
+            Padding(
+              padding: const EdgeInsets.all(32.0),
+              child: Form(
+                child: Column(
+                  children: <Widget>[
+                    TextFormField(
+                      decoration: InputDecoration(
+                          labelText: 'Email',
+                          icon: Icon(
+                            Icons.email,
+                            color: Theme.of(context).primaryColorLight,
+                          )),
+                      keyboardType: TextInputType.emailAddress,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 8.0, bottom: 32),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                            labelText: 'Senha',
+                            icon: Icon(
+                              Icons.lock,
+                              color: Theme.of(context).primaryColorLight,
+                            )),
+                        obscureText: true,
+                      ),
+                    ),
+                    ElevatedButton(
+                      style: TextButton.styleFrom(
+                          backgroundColor: Theme.of(context).primaryColor),
                       onPressed: () {},
-                      child: Row(
-                          children: [Icon(Icons.person), Text('Criar Conta')], mainAxisSize: MainAxisSize.min,))
-                ],
+                      child: Text('Entrar'.toUpperCase()),
+                    ),
+                    TextButton.icon(
+                      style: TextButton.styleFrom(
+                          primary: Theme.of(context).primaryColor),
+                      onPressed: () {},
+                      icon: Icon(Icons.person),
+                      label: Text('Criar Conta'),
+                    )
+                  ],
+                ),
               ),
             )
           ],
@@ -41,3 +61,5 @@ class LoginPage extends StatelessWidget {
     );
   }
 }
+
+
