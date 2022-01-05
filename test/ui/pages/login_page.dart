6227b0760cf32598fc99effb2d6259a88f2694db
Paulo Freire <paulo.freiire@hotmail.com>
2022-01-05 01:51:44 -0300

    'move page initialization to a helper method

--- test/ui/pages/login_page.dart
+++ test/ui/pages/login_page.dart
@@ -3,17 +3,30 @@ import 'package:flutter_tdd_clean/ui/pages/pages.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  Widget createWidgetForTesting({Widget child}) {
-    return MaterialApp(
-      home: child,
-    );
+  Future loadPage(WidgetTester tester) async {
+    final loginPage = MaterialApp(home: LoginPage());
+    await tester.pumpWidget(loginPage);
   }
 
   testWidgets("Should load with correct initial states",
       (WidgetTester tester) async {
-    final loginPage = createWidgetForTesting(child: LoginPage());
+    await loadPage(tester);
 
-    await tester.pumpWidget(loginPage);
+    final emailTextChildren = find.descendant(
+        of: find.bySemanticsLabel('Email'), matching: find.byType(Text));
+    expect(emailTextChildren, findsOneWidget);
+
+    final passwordTextChildren = find.descendant(
+        of: find.bySemanticsLabel('Senha'), matching: find.byType(Text));
+    expect(passwordTextChildren, findsOneWidget);
+
+    final button = tester.widget<ElevatedButton>(find.byType(ElevatedButton));
+    expect(button.onPressed, null);
+  });
+
+  testWidgets("Should call validate with correct values",
+      (WidgetTester tester) async {
+    await loadPage(tester);
 
     final emailTextChildren = find.descendant(
         of: find.bySemanticsLabel('Email'), matching: find.byType(Text));
