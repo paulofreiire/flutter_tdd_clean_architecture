36d2fa08ea0431ceec28fc671d4ee447d312fcf6
Paulo Freire <paulo.freiire@hotmail.com>
2022-05-03 15:41:23 -0300

    move SpinnderDialog to its own file

--- lib/ui/pages/login/login_page.dart
+++ lib/ui/pages/login/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_tdd_clean/ui/components/components.dart';
 import 'package:flutter_tdd_clean/ui/components/headline1.dart';
 import 'package:flutter_tdd_clean/ui/pages/pages.dart';
 import '../../components/login_header.dart';
@@ -19,26 +20,9 @@ class _LoginPageState extends State<LoginPage> {
       body: Builder(builder: (context) {
         widget.presenter.isLoadingStream.listen((isLoading) {
           if (isLoading)
-            showDialog(
-                context: context,
-                barrierDismissible: false,
-                builder: (context) {
-                  return SimpleDialog(
-                    children: [
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          CircularProgressIndicator(),
-                          SizedBox(
-                            height: 10,
-                          ),
-                          Text("Aguarde...", textAlign: TextAlign.center),
-                        ],
-                      )
-                    ],
-                  );
-                });
-          else if (Navigator.canPop(context)) Navigator.of(context).pop();
+            showLoading(context);
+          else
+            hideLoading(context);
         });
 
         widget.presenter.mainErrorStream.listen((error) {
