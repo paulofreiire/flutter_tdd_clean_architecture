ac804ee652ce880a2399cba564810d9ec04aa423
Paulo Freire <paulo.freiire@hotmail.com>
2022-05-04 11:23:25 -0300

    move LoginButton to its own file

---
+++ lib/ui/pages/login/components/login_button.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../login_presenter.dart';
+
+class LoginButton extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    final presenter = Provider.of<LoginPresenter>(context);
+
+    return StreamBuilder<bool>(
+        stream: presenter.isFormValidStream,
+        builder: (context, snapshot) {
+          return ElevatedButton(
+            style: TextButton.styleFrom(
+                backgroundColor:
+                Theme.of(context).primaryColor),
+            onPressed: snapshot.data == true
+                ? presenter.auth
+                : null,
+            child: Text('Entrar'.toUpperCase()),
+          );
+        });
+  }
+}
--- lib/ui/pages/login/login_page.dart
+++ lib/ui/pages/login/login_page.dart
@@ -50,19 +50,7 @@ class _LoginPageState extends State<LoginPage> {
                           padding: const EdgeInsets.only(top: 8.0, bottom: 32),
                           child: PasswordInput(),
                         ),
-                        StreamBuilder<bool>(
-                            stream: widget.presenter.isFormValidStream,
-                            builder: (context, snapshot) {
-                              return ElevatedButton(
-                                style: TextButton.styleFrom(
-                                    backgroundColor:
-                                        Theme.of(context).primaryColor),
-                                onPressed: snapshot.data == true
-                                    ? widget.presenter.auth
-                                    : null,
-                                child: Text('Entrar'.toUpperCase()),
-                              );
-                            }),
+                        LoginButton(),
                         TextButton.icon(
                           style: TextButton.styleFrom(
                               primary: Theme.of(context).primaryColor),
@@ -88,4 +76,5 @@ class _LoginPageState extends State<LoginPage> {
     widget.presenter.dispose();
   }
 }
+
 
