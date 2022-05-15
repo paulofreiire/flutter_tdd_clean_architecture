10872db6adf8a60684862f8f8b7b58851b201bce
Paulo Freire <paulo.freiire@hotmail.com>
2022-05-15 00:59:59 -0300

    ensure EmailErrorStream will not emit duplicated event

--- test/presentation/presenters/stream_login_presenter.dart
+++ test/presentation/presenters/stream_login_presenter.dart
@@ -4,7 +4,6 @@ import 'package:flutter_tdd_clean/presentation/protocols/protocols.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
-
 class ValidationSpy extends Mock implements Validation {}
 
 void main() {
@@ -36,8 +35,10 @@ void main() {
   test('Should emit email error if validation fails', () {
     mockValidation(value: 'error');
 
-    expectLater(sut.emailErrorStream, emits('error'));
+    sut.emailErrorStream
+        .listen(expectAsync1((error) => expect(error, 'error')));
 
+    sut.validateEmail(email);
     sut.validateEmail(email);
   });
 }
