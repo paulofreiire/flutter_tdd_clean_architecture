f6e5c273937b54c1fdd7a9d16ce2f68ada882349
Paulo Freire <paulo.freiire@hotmail.com>
2022-06-03 14:45:32 -0300

    ensure EmailValidation return error if email is invalid

---
+++ test/validation/validators/email_validation_test.dart
@@ -0,0 +1,40 @@
+import 'package:flutter_tdd_clean/validation/protocols/field_validation.dart';
+import 'package:test/test.dart';
+
+class EmailValidation implements FieldValidation {
+  final String field;
+
+  EmailValidation(this.field);
+
+  String validate(String value) {
+    final regex = RegExp(
+        r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+    final isValid = value?.isNotEmpty != true || regex.hasMatch(value);
+
+    return isValid ? null : "Campo inválido";
+  }
+}
+
+void main() {
+  EmailValidation sut;
+
+  setUp(() {
+    sut = EmailValidation('any_field');
+  });
+
+  test('Should return null if email is empty', () {
+    expect(sut.validate(''), null);
+  });
+
+  test('Should return null if email is null', () {
+    expect(sut.validate(null), null);
+  });
+
+  test('Should return null if email is valid', () {
+    expect(sut.validate('email@email.com'), null);
+  });
+
+  test('Should return error if email is invalid', () {
+    expect(sut.validate('email'), "Campo inválido");
+  });
+}
---
+++ test/validation/validators/requried_field_validation_test.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_tdd_clean/validation/validators/validators.dart';
+import 'package:test/test.dart';
+
+void main() {
+  RequiredFieldValidation sut;
+
+  setUp(() {
+    sut = RequiredFieldValidation('any_field');
+  });
+
+  test('Should return null if value is not empty', () {
+    expect(sut.validate('any_value'), null);
+  });
+
+  test('Should return error if value is empty', () {
+    expect(sut.validate(''), 'Campo obrigatório');
+  });
+
+  test('Should return error if value is null', () {
+    expect(sut.validate(null), 'Campo obrigatório');
+  });
+}
